ade8d303399a679fc45c29b11bc2c12d84895f6d
Cody Goodman <codygman.consulting@gmail.com>
2019-10-23 19:04:30 -0500

    install home manager

--- test.sh
+++ test.sh
@@ -1,1 +1,4 @@
-exit 0
+nix-channel --add https://github.com/rycee/home-manager/archive/master.tar.gz home-manager
+nix-channel --update
+export NIX_PATH=$HOME/.nix-defexpr/channels${NIX_PATH:+:}$NIX_PATH
+nix-shell '<home-manager>' -A install
