f223a6b0cc6a9a6a5fbbfb507d4b093a308b6a79
codygman <codygman.consulting@gmail.com>
2019-10-23 21:40:47 -0500

    add quite a few things

--- my-tests.el
+++ my-tests.el
@@ -6,3 +6,37 @@
 
 (ert-deftest org-mode-installed ()
   (should (string-equal "9.2.6" org-version)))
+
+(ert-deftest helm-installed ()
+  (should (fboundp 'helm-mode)))
+
+(ert-deftest evil-installed ()
+  (should (fboundp 'evil-version)))
+
+(ert-deftest evil-collection-installed ()
+  (should (fboundp 'evil-collection-init)))
+
+(ert-deftest avy-installed ()
+  (should (fboundp 'avy-goto-char-timer)))
+
+(ert-deftest magit-installed ()
+  (should (fboundp 'magit-version)))
+
+(ert-deftest helpful-installed ()
+  (should (fboundp 'helpful-callable)))
+
+(ert-deftest helm-swoop-installed ()
+  (should (fboundp 'helm-swoop)))
+
+(ert-deftest winner-mode-active ()
+  (bound-and-true-p winner-mode))
+
+;; more specific tests
+
+;; TODO RET works in grep buffers
+;; (with-eval-after-load 'evil-maps
+;;   (define-key evil-motion-state-map (kbd "SPC") nil)
+;;   (define-key evil-motion-state-map (kbd "RET") nil)
+;;   (define-key evil-motion-state-map (kbd "TAB") nil))
+;; )
+
