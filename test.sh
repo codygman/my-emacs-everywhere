a80409222ff6d568ba61004f9a1ab901330b0969
codygman <codygman.consulting@gmail.com>
2019-10-23 22:14:52 -0500

    actually use home-manager config

--- test.sh
+++ test.sh
@@ -3,4 +3,8 @@ nix-channel --update
 export NIX_PATH=$HOME/.nix-defexpr/channels${NIX_PATH:+:}$NIX_PATH
 nix-shell '<home-manager>' -A install
 
+mkdir -p ~/.config/
+cp -vR nixpkgs ~/.config
+home-manager switch
+
 emacs -Q -batch --load load-init-then-run-ert.el
