e61389bd187e1cc34fdd679a0fe2bb519c0e53fd
codygman <codygman.consulting@gmail.com>
2019-11-27 14:26:06 -0600

    sit for longer to wait for ghci result

--- my-tests.el
+++ my-tests.el
@@ -94,8 +94,8 @@
       (message "inserted text 'main'")
       (haskell-interactive-mode-return)
       (message "haskell return")
-      (sit-for 1)
-      (message "sit for 1")
+      (sit-for 3)
+      (message "sit for 3")
       (message "START ghci repl looks like (after execute)")
       (message
        (format "%s" (substring-no-properties (buffer-substring (point-min) (point-max)))))
