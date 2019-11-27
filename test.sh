aff0b8e72c23b583ec87442ccbaccf10e62e55b2
Cody Goodman <codygman.consulting@gmail.com>
2019-11-26 18:27:49 -0600

    wip add cachix to travis

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 nix-channel --add https://github.com/rycee/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
 export NIX_PATH=$HOME/.nix-defexpr/channels${NIX_PATH:+:}$NIX_PATH
+nix-env -iA cachix -f https://cachix.org/api/v1/install
 
 nix-shell '<home-manager>' -A install
 
