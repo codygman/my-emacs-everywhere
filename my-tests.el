1ed6f53d860216b53d3174495901429457a4d489
codygman <codygman.consulting@gmail.com>
2019-11-27 04:21:17 -0600

    fix error; add more logs

--- my-tests.el
+++ my-tests.el
@@ -67,7 +67,7 @@
     (message "sat for 2")
     (message "START ghci repl looks like")
     (message
-     (format "%s" (substring-no-properties (buffer-substring beg end))))
+     (format "%s" (substring-no-properties (buffer-substring (point-min) (point-max)))))
     (message "END ghci repl looks like")
     (goto-char (point-max))
     (message "went to point max")
@@ -79,6 +79,10 @@
     (message "haskell return")
     (sit-for 1)
     (message "sit for 1")
+    (message "START ghci repl looks like (after execute)")
+    (message
+     (format "%s" (substring-no-properties (buffer-substring (point-min) (point-max)))))
+    (message "END ghci repl looks like (after execute)")
     (evil-previous-line)
     (message "go to previous line")
     (get-substring-from-line)
