a51a44d1f82e5eca8a12133da0654c37b0b20269
codygman <codygman.consulting@gmail.com>
2019-11-27 01:57:18 -0600

    see if emacs straight packages install from cache
    that's the only purpose of this commit

--- test.sh
+++ test.sh
@@ -8,12 +8,11 @@ if [ $TRAVIS_OS_NAME = 'osx' ]; then
     echo "trusted-users = root $USER" | sudo tee -a /etc/nix/nix.conf && sudo pkill nix-daemon
 fi
 nix-shell '<home-manager>' -A install
-cachix use codygman4 # did cachix work?
+cachix use codygman4
 
 mkdir -p ~/.config/
 cp -vR nixpkgs ~/.config
 
-# try pushing emacs.nix to cachix
 pushd ~/.config/nixpkgs
 nix-build emacs.nix | cachix push codygman4
 popd
