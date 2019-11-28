37e12a1f661b838b7420684f8d0e95c5d7019050
codygman <codygman.consulting@gmail.com>
2019-11-28 00:33:26 -0600

    remove goto-char call

--- my-tests.el
+++ my-tests.el
@@ -4,11 +4,7 @@
     "~/build/codygman/my-emacs-everywhere/"))
 
 (defun open-lorem-ipsum-goto-end-scroll-up-return-char-position ()
-  (save-excursion
-    (find-file "testdata/loremipsum.txt")
-    (goto-char (point-max))
-    (call-interactively 'evil-scroll-up)
-    (point)))
+    (call-interactively 'evil-scroll-up))
 
 ;; START evil
 (ert-deftest evil-ctrl-u-scrolls-up ()
