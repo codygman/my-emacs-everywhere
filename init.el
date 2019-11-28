016c5befb8adcb7f0fcf487814c08999972472e2
codygman <codygman.consulting@gmail.com>
2019-11-28 00:42:04 -0600

    move things into init.el

--- init.el
+++ init.el
@@ -1,1 +1,19 @@
-(load "~/.emacs.d/emacs-config.el")
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(package-initialize)
+
+;; Bootstrap `use-package'
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(use-package evil
+  :ensure t
+  :init
+  (setq evil-want-integration t
+	evil-want-keybinding nil
+	evil-want-C-u-scroll t)
+  :config
+  (setq evil-symbol-word-search t)
+  (evil-mode 1))
