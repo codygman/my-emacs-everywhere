bbe7c98792bcd4f789662c5e3882886ee549bf44
codygman <codygman.consulting@gmail.com>
2019-11-27 22:58:10 -0600

    get test running locally

--- my-tests.el
+++ my-tests.el
@@ -3,7 +3,13 @@
       "~/.emacs.d"
     "~/build/codygman/my-emacs-everywhere/"))
 
-
+(defun open-lorem-ipsum-goto-end-scroll-up-return-char-position ()
+  (save-excursion
+    (find-file "testdata/loremipsum.txt")
+    (goto-char (point-max))
+    (require 'general)
+    (call-interactively (general-simulate-key "C-u"))
+    (point)))
 
 ;; START evil
 (ert-deftest evil-ctrl-u-scrolls-up ()
