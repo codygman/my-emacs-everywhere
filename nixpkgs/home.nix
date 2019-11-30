996e631765eb4b46a668e0f05117b4be6f9fd3c2
codygman <codygman.consulting@gmail.com>
2019-11-29 18:54:57 -0600

    minimal home.nix to only install emacs

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -15,84 +15,10 @@ in
       enable = true;
       package = emacsHEAD;
     };
-    ssh = mkIf (builtins.getEnv "TRAVIS_OS_NAME" == "" && stdenv.isLinux) {
-      enable = true;
-      controlPath = "~/.ssh/master-%C";
-    };
-    htop.enable = true;
-    firefox = mkIf stdenv.isLinux {
-      # todo install with home-manager
-      enable = true;
-    };
-    jq.enable = true;
-    vim.enable = true;
-    git = {
-      enable = true;
-      userName = "codygman";
-      userEmail = "codygman.consulting@gmail.com";
-    };
-    gpg = mkIf (builtins.getEnv "TRAVIS_OS_NAME" == "") {
-      enable = true;
-    };
   };
 
   home = {
-    packages = with pkgs; [
-      ripgrep
-      gnumake
-      fd
-      ghc
-    ] ++ (if builtins.getEnv "TRAVIS_OS_NAME" == "" then [
-      feh
-      dmenu
-      haskellPackages.lens
-      haskellPackages.pandoc
-      haskellPackages.ghcid
-      haskellPackages.hlint
-      haskellPackages.brittany
-      haskellPackages.hpack
-      haskellPackages.cabal-install
-      stack
-      source-code-pro
-      cabal2nix
-    ] else []);
-  };
-
-  systemd.user.startServices = if stdenv.isLinux then true else false;
-
-  home.keyboard = mkIf stdenv.isLinux {
-    # TODO test to see if this works on osx too
-    layout = "us";
-    options = [
-      "ctrl:nocaps"
-    ];
-  };
-
-  home.sessionVariables = {
-    EDITOR = "emacsclient --create-frame --alternate-editor emacs";
-  };
-
-  accounts.email.accounts = {
-    "codygman.consulting@gmail.com" = {
-      primary = true;
-      address = "codygman.consulting@gmail.com";
-      userName = "codygman.consulting@gmail.com";
-      realName = "Cody Goodman";
-    };
-  };
-
-  services = {
-    syncthing = mkIf (builtins.getEnv "TRAVIS_OS_NAME" == "" && stdenv.isLinux) {
-      enable = true;
-    };
-    gpg-agent = mkIf (builtins.getEnv "TRAVIS_OS_NAME" == "") {
-      enable = true;
-      defaultCacheTtl = 600;
-      enableSshSupport = true;
-    };
   };
 
   
-
-
 }
