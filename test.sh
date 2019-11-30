1abf6ceab8b1a7cb48f442dd2b5e30d5a6ee4dad
codygman <codygman.consulting@gmail.com>
2019-11-29 18:12:04 -0600

    split up install/test steps

--- test.sh
+++ test.sh
@@ -1,22 +1,2 @@
-nix-channel --add https://github.com/rycee/home-manager/archive/master.tar.gz home-manager
-nix-channel --update
-export NIX_PATH=$HOME/.nix-defexpr/channels${NIX_PATH:+:}$NIX_PATH
-nix-env -iA cachix -f https://cachix.org/api/v1/install
-
-# see if the cached build that succeeded is actually used
-if [ $TRAVIS_OS_NAME = 'osx' ]; then
-    echo "trusted-users = root $USER" | sudo tee -a /etc/nix/nix.conf && sudo pkill nix-daemon
-fi
-nix-shell '<home-manager>' -A install
-cachix use codygman4
-
-mkdir -p ~/.config/
-cp -vR nixpkgs ~/.config
-
-pushd ~/.config/nixpkgs
-nix-build emacs.nix | cachix push codygman4
-popd
-home-manager switch
-
 emacs -nw -Q -l init.el -l my-tests.el --eval "(tests-run)"
 
