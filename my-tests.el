102b68aa7570aac8b2ca20d7544a1af62c2fd718
codygman <codygman.consulting@gmail.com>
2019-11-30 06:20:14 -0600

    Revert "temporarily remove failing tests"
    This reverts commit 26a43a8c81528ef0ab11f3557566851b4a3a1f24.

--- my-tests.el
+++ my-tests.el
@@ -25,7 +25,6 @@
   (if (eq nil (getenv "TRAVIS_OS_NAME"))
       "~/.emacs.d/"
     "~/build/codygman/my-emacs-everywhere/"))
-
 (ert-deftest version-check ()
   (should (string-equal "27.0.50" emacs-version)))
 
@@ -38,6 +37,45 @@
 (ert-deftest helm-installed ()
   (should (fboundp 'helm-mode)))
 
+(ert-deftest evil-installed ()
+  (should (fboundp 'evil-version)))
+
+(ert-deftest evil-collection-installed ()
+  (should (fboundp 'evil-collection-init)))
+
+(ert-deftest avy-installed ()
+  (should (fboundp 'avy-goto-char-timer)))
+
+(ert-deftest magit-installed ()
+  (should (fboundp 'magit-version)))
+
+(ert-deftest helpful-installed ()
+  (should (fboundp 'helpful-callable)))
+
+(ert-deftest helm-swoop-installed ()
+  (should (fboundp 'helm-swoop)))
+
+(ert-deftest winner-mode-active ()
+  (bound-and-true-p winner-mode))
+
+(ert-deftest host-gnumake-installed ()
+  (should (not (eq nil (executable-find "make")))))
+
+(ert-deftest host-rg-installed ()
+  (should (not (eq nil (executable-find "rg")))))
+
+(ert-deftest host-fd-installed ()
+  (should (not (eq nil (executable-find "fd")))))
+
+;; more specific tests
+(ert-deftest searching-works-as-expected ()
+  (should (not (eq nil (executable-find "rg"))))
+  (should (string-equal "rg --color=always --smart-case --no-heading --line-number %s %s %s" helm-grep-ag-command))
+  (require 'helm-grep)
+  ;; (should (string-equal "helm-do-grep-ag"
+  ;; 			(key-binding (kbd "SPC g g"))))
+  )
+
 (ert-deftest trivial-helm-example-works-as-expected ()
   (should (string-equal
 	   "john"
@@ -49,3 +87,62 @@
 			`((name . "Simple helm names example")
 			  (candidates . ,(list "jane" "john"))
 			  (action . (lambda (candidate) (helm-marked-candidates))))))))))
+
+;; (ert-deftest haskell-mode-enabled-opening-haskell-file ()
+;;   (find-file (format "%s/testdata/simple-haskell-project/Main.hs" (my-emacs-everywhere-directory)))
+;;   (should (eq 'haskell-mode (derived-mode-p 'haskell-mode))))
+
+  (defun get-substring-from-line ()
+    "Copy the whole line that point is on and move to the beginning of the next line.
+    Consecutive calls to this command append each line to the
+    kill-ring."
+    (interactive)
+    (let ((beg (line-beginning-position 1))
+	  (end (line-beginning-position 2)))
+      (string-trim (substring-no-properties (buffer-substring beg end)))))
+
+  ;; (defun load-simple-hs-file-and-return-ghci-evald-main ()
+  ;;   (save-excursion
+  ;;     (find-file (format "%s/testdata/simple-haskell-project/Main.hs" (my-emacs-everywhere-directory)))
+  ;;     (message "found file")
+  ;;     (haskell-process-load-file)
+  ;;     (message "loaded file")
+  ;;     (switch-to-buffer "*simple-haskell-project*")
+  ;;     (message "switched to buffer")
+  ;;     (sit-for 2)
+  ;;     (message "sat for 2")
+  ;;     (message "START ghci repl looks like")
+  ;;     (message
+  ;;      (format "%s" (substring-no-properties (buffer-substring (point-min) (point-max)))))
+  ;;     (message "END ghci repl looks like")
+  ;;     (goto-char (point-max))
+  ;;     (message "went to point max")
+  ;;     (evil-append-line 1)
+  ;;     (message "insert mode at end of line")
+  ;;     (insert "main")
+  ;;     (message "inserted text 'main'")
+  ;;     (haskell-interactive-mode-return)
+  ;;     (message "haskell return")
+  ;;     (sit-for 3)
+  ;;     (message "sit for 3")
+  ;;     (message "START ghci repl looks like (after execute)")
+  ;;     (message
+  ;;      (format "%s" (substring-no-properties (buffer-substring (point-min) (point-max)))))
+  ;;     (message "END ghci repl looks like (after execute)")
+  ;;     (evil-previous-line)
+  ;;     (message "go to previous line")
+  ;;     (get-substring-from-line)
+  ;;     ))
+
+  ;; (ert-deftest haskell-mode-ghci-loads-file-and-can-execute ()
+  ;;   (should (string-equal
+  ;; 	     "Hello, Haskell!"
+  ;; 	     (load-simple-hs-file-and-return-ghci-evald-main))))
+
+  ;; TODO RET works in grep buffers
+  ;; (with-eval-after-load 'evil-maps
+  ;;   (define-key evil-motion-state-map (kbd "SPC") nil)
+  ;;   (define-key evil-motion-state-map (kbd "RET") nil)
+  ;;   (define-key evil-motion-state-map (kbd "TAB") nil))
+  ;; )
+
