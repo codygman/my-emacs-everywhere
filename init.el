bc0a6bae2af9bf9ee2b6c21ded4bf33d626a43a9
codygman <codygman.consulting@gmail.com>
2019-12-01 00:43:54 -0600

    update how I install org-plus-contrib

--- init.el
+++ init.el
@@ -19,7 +19,8 @@
 
 (setq straight-use-package-by-default t)
 
-(straight-use-package '(org-plus-contrib :includes (org)))
+(straight-use-package 'org-plus-contrib)
+(straight-use-package '(org :local-repo nil))
 (straight-use-package 'use-package)
 
 (when (file-exists-p "/data/data/com.termux/files/usr/bin/termux-info")
