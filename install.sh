aa79789ef06d42b60f89fccd3df1d4850294284b
Cody Goodman <codygman.consulting@gmail.com>
2019-12-01 04:15:11 -0600

    Revert "move more into travis.yml for timing info"
    This reverts commit cd42db008f9796165a179ea3451e838a6100992b.

--- install.sh
+++ install.sh
@@ -7,3 +7,12 @@ nix-env -iA cachix -f https://cachix.org/api/v1/install
 if [ $TRAVIS_OS_NAME = 'osx' ]; then
     echo "trusted-users = root $USER" | sudo tee -a /etc/nix/nix.conf && sudo pkill nix-daemon
 fi
+nix-shell '<home-manager>' -A install
+cachix use codygman4
+
+mkdir -p ~/.config/
+cp -vR nixpkgs ~/.config
+
+pushd ~/.config/nixpkgs
+nix-build emacs.nix | cachix push codygman4
+popd
