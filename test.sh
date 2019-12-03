1a93b99092e07fff0629bb1a9fe875ce6cfcedce
codygman <codygman.consulting@gmail.com>
2019-12-02 19:15:17 -0600

    use work EMACSFOR for osx and personal for linux

--- test.sh
+++ test.sh
@@ -1,3 +1,8 @@
-cp -Rv ~/build/codygman/my-emacs-everywhere/snippets ~/.emacs.d/snippets
-emacs -nw --load load-init-then-run-ert.el
+#!/bin/bash
 
+cp -Rv ~/build/codygman/my-emacs-everywhere/snippets ~/.emacs.d/snippets
+if [ "$TRAVIS_OS_NAME" = "osx" ]; then
+    EMACSFOR="WORK" emacs -nw --load load-init-then-run-ert.el
+elif [ "$TRAVIS_OS_NAME" = "linux" ]; then
+    EMACSFOR="PERSONAL" emacs -nw --load load-init-then-run-ert.el
+fi
