bc72c4ba8b78c44dca21f036a4310e4bfd5547cc
codygman <codygman.consulting@gmail.com>
2020-01-03 06:44:24 -0600

    add nixify function to bash/zsh profile

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -110,11 +110,47 @@ in
       ".bashrc" = {
         text = ''
           eval "$(direnv hook bash)"
+          nixify() {
+            if [ ! -e ./.envrc ]; then
+              echo "use nix" > .envrc
+              direnv allow
+            fi
+            if [ ! -e default.nix ]; then
+              cat > default.nix <<'EOF'
+          with import <nixpkgs> {};
+          stdenv.mkDerivation {
+            name = "env";
+            buildInputs = [
+              bashInteractive
+            ];
+          }
+          EOF
+              ${EDITOR:-vim} default.nix
+            fi
+          }
         '';
       };
       ".zshrc" = {
         text = ''
           eval "$(direnv hook zsh)"
+          nixify() {
+            if [ ! -e ./.envrc ]; then
+              echo "use nix" > .envrc
+              direnv allow
+            fi
+            if [ ! -e default.nix ]; then
+              cat > default.nix <<'EOF'
+          with import <nixpkgs> {};
+          stdenv.mkDerivation {
+            name = "env";
+            buildInputs = [
+              bashInteractive
+            ];
+          }
+          EOF
+              ${EDITOR:-vim} default.nix
+            fi
+          }
         '';
       };
       ".direnvrc" = {
