dd8262b490443fe729a8ee11251b16f529af14fa
Cody Goodman <cody@codygman.dev>
2020-03-15 18:19:13 -0500

    WIP fix test local script
    might already be fixed

--- test-local.sh
+++ test-local.sh
@@ -5,22 +5,29 @@ export CUSTOMHOME="/tmp/my-emacs-everywhere"
 export EMACSD="$CUSTOMHOME/.emacs.d"
 
 echo "restore straight cache if exists"
-cp -R $EMACSD/straight $EMACSD/straight  2> /dev/null
-
-echo "removing EMACSD: $EMACSD"
-rm -r $EMACSD 2> /dev/null
-echo "DONE removing EMACSD: $EMACSD"
-
 mkdir -p $CUSTOMHOME
-cp -R ~/my-emacs-everywhere/ $EMACSD
-cd $EMACSD
-# tree
-emacs -nw --load load-init-then-run-ert.el
-
-echo "save straight cache"
-cp -R $EMACSD/straight "$CUSTOMHOME"
+ls -alrth $CUSTOMHOME/straight
+cp -R $CUSTOMHOME/straight $EMACSD/straight  2> /dev/null
 
 echo "removing EMACSD: $EMACSD"
 rm -rf $EMACSD 2> /dev/null
 echo "DONE removing EMACSD: $EMACSD"
-echo "left /tmp/my-emacs-everywhere to preserve straight install cache for next local run"
+
+cp -r ~/my-emacs-everywhere/ $EMACSD
+cd $EMACSD
+# tree
+export EMACSFOR="PERSONAL"
+HOME=$CUSTOMHOME emacs -nw --load load-init-then-run-ert.el
+# cat test-results.txt
+
+# echo "save straight cache"
+# mkdir -p straight
+# echo "$EMACSD/straight"
+# echo  "$CUSTOMHOME/straight"
+# cp -R "$EMACSD/straight" "$CUSTOMHOME/straight"
+# ls -larth
+
+# echo "removing EMACSD: $EMACSD"
+# rm -rf $EMACSD 2> /dev/null
+# echo "DONE removing EMACSD: $EMACSD"
+# echo "left /tmp/my-emacs-everywhere to preserve straight install cache for next local run"
