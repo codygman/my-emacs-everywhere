ace788f5a103dec02ade795b2bbfabc65553656b
codygman <codygman.consulting@gmail.com>
2020-03-30 04:28:13 +0000

    Revert "try using myemacs instead of emacs-overlay"
    This reverts commit f5c305527dcc52b4897b1ce2aaff224f3418ada9.

--- nixpkgs/doomemacs.nix
+++ nixpkgs/doomemacs.nix
@@ -1,11 +1,11 @@
 let
-  myemacs = import ./emacs.nix;
-  pkgs = import <nixpkgs> { };
+  emacs-overlay = builtins.fetchTarball "https://github.com/nix-community/emacs-overlay/archive/52b9fd468cd45f85c43f9b623ed2854971d8e1ad.tar.gz";
+  pkgs = import <nixpkgs> { overlays = [ (import emacs-overlay) ]; };
   nix-doom-emacs = builtins.fetchTarball "https://github.com/vlaci/nix-doom-emacs/archive/master.tar.gz";
 in
   with pkgs; callPackage nix-doom-emacs {
     bundledPackages = false;
-    emacsPackages = emacsPackagesFor myemacs;
+    emacsPackages = emacsPackagesFor emacsGit;
     doomPrivateDir = ./doom.d;
   }
 
