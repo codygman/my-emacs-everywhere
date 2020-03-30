347c3cd98a5772a3f729f159c40dec4a5682f809
codygman <codygman.consulting@gmail.com>
2020-03-30 17:49:11 -0500

    fix emacs-overlay

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -5,6 +5,7 @@ with lib;
 
 let
   pkgs = import <nixpkgs> { overlays = [ (import emacs-overlay) ]; };
+  emacs-overlay = builtins.fetchTarball "https://github.com/nix-community/emacs-overlay/archive/52b9fd468cd45f85c43f9b623ed2854971d8e1ad.tar.gz";
   doomemacs = import ./doomemacs.nix;
 in
 {
@@ -19,7 +20,7 @@ in
   };
 
   home = {
-    packages = with pkgs; [ doomemacs ];
+    packages = with pkgs; [ doomemacs fd ripgrep ];
     file = {
     ".emacs.d/init.el".text = ''
      (load "default.el")
