c2eeceb28447e24e871cd2a23e41116824971b1d
codygman <codygman.consulting@gmail.com>
2020-03-30 18:21:39 -0500

    add custom module loading

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -4,11 +4,24 @@ with import <nixpkgs> {};
 with lib;
 
 let
+  myEnv = builtins.getEnv "MYENV";
   pkgs = import <nixpkgs> { overlays = [ (import emacs-overlay) ]; };
   emacs-overlay = builtins.fetchTarball "https://github.com/nix-community/emacs-overlay/archive/52b9fd468cd45f85c43f9b623ed2854971d8e1ad.tar.gz";
   doomemacs = import ./doomemacs.nix;
 in
 {
+  imports = if myEnv != ""
+            then if myEnv == "personal" then
+              lib.info "loading PERSONAL home manager environment"
+                [ ~/Sync/nix-home-manager-config/personal.nix ]
+                 else
+                   if myEnv == "work" then
+                     lib.info "loading WORK home manager environment"
+                       [ ~/Sync/nix-home-manager-config/work.nix ]
+                   else
+                     lib.warn "MYENV is not one of 'personal' or 'work', ONLY core home environment will be available!" []
+            else
+              lib.warn "MYENV not specified, ONLY core home environment will be available!" [];
 
   programs = {
     home-manager.enable = true;
