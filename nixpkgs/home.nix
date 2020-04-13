a521b96ab0eac1b7d4d48cab092b7c761330eff4
codygman <cody@codygman.dev>
2020-04-13 00:06:06 -0500

    WIP start using niv to pin home-manager
    WIP because I'm not 100% sure it's working
    
    When I switched to something super old like 18.09 though it seemed to work

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -1,34 +1,36 @@
-{ config, pkgs, home, ... }:
-
-with import <nixpkgs> {};
-with lib;
-
+{ pkgs, sources ? import ./sources.nix, ...}:
+with
+  { overlay = _: pkgs:
+      { niv = import sources.niv {};    # use the sources :)
+      };
+  };
 let
+  sources = import ./nix/sources.nix;
   myEnv = builtins.getEnv "MYENV";
-  pkgs = import <nixpkgs> { overlays = [ (import emacs-overlay) ]; };
+  pkgs = import sources.nixpkgs { overlays = [ overlay ] ; config = {};   };
   emacs-overlay = builtins.fetchTarball "https://github.com/nix-community/emacs-overlay/archive/52b9fd468cd45f85c43f9b623ed2854971d8e1ad.tar.gz";
   doomemacs = import ./doomemacs.nix;
 in
 {
   imports = if myEnv != ""
             then if myEnv == "personal" then
-              lib.info "loading PERSONAL home manager environment"
+              pkgs.lib.info "loading PERSONAL home manager environment"
                 [ ~/Sync/nix-home-manager-config/personal.nix ]
                  else
                    if myEnv == "work" then
-                     lib.info "loading WORK home manager environment"
+                     pkgs.lib.info "loading WORK home manager environment"
                        [ ~/Sync/nix-home-manager-config/work.nix ]
                    else
-                     lib.warn "MYENV is not one of 'personal' or 'work', ONLY core home environment will be available!" []
+                     pkgs.lib.warn "MYENV is not one of 'personal' or 'work', ONLY core home environment will be available!" []
             else
-              lib.warn "MYENV not specified, ONLY core home environment will be available!" [];
+              pkgs.lib.warn "MYENV not specified, ONLY core home environment will be available!" [];
 
   programs = {
     home-manager.enable = true;
     git = {
       enable = true;
       userName = "codygman";
-      userEmail = lib.mkDefault "codygman.consulting@gmail.com";
+      userEmail = pkgs.lib.mkDefault "codygman.consulting@gmail.com";
     };
     direnv = {
       enable = true;
